1e13e2e7cfa77c6a4376f5d0e10d61c7210de8a7
Dan Knight <danknight@mednet.ucla.edu>
2025-03-19 15:20:23 -0700

    Sample normalization regression cases

---
+++ tests/testthat/test-sample.content.normalization.R
@@ -0,0 +1,74 @@
+test_that(
+    'sample.content.normalization regression case "housekeeping sum"', {
+        load('data/NanoStringNorm.Rda');
+
+        result <- sample.content.normalization(
+            NanoStringNorm.test.data$inputs$x,
+            NanoStringNorm.test.data$inputs$anno,
+            'housekeeping.sum',
+            verbose = FALSE
+            );
+
+        expect_equivalent(result, NanoStringNorm.test.data$outputs$housekeeping.sum);
+        }
+    );
+
+test_that(
+    'sample.content.normalization regression case "housekeeping geo mean"', {
+        load('data/NanoStringNorm.Rda');
+
+        result <- sample.content.normalization(
+            NanoStringNorm.test.data$inputs$x,
+            NanoStringNorm.test.data$inputs$anno,
+            'housekeeping.geo.mean',
+            verbose = FALSE
+            );
+
+        expect_equivalent(result, NanoStringNorm.test.data$outputs$housekeeping.geo.mean);
+        }
+    );
+
+test_that(
+    'sample.content.normalization regression case "top mean"', {
+        load('data/NanoStringNorm.Rda');
+
+        result <- sample.content.normalization(
+            NanoStringNorm.test.data$inputs$x,
+            NanoStringNorm.test.data$inputs$anno,
+            'top.mean',
+            verbose = FALSE
+            );
+
+        expect_equivalent(result, NanoStringNorm.test.data$outputs$top.mean);
+        }
+    );
+
+test_that(
+    'sample.content.normalization regression case "top geo mean"', {
+        load('data/NanoStringNorm.Rda');
+
+        result <- sample.content.normalization(
+            NanoStringNorm.test.data$inputs$x,
+            NanoStringNorm.test.data$inputs$anno,
+            'top.geo.mean',
+            verbose = FALSE
+            );
+
+        expect_equivalent(result, NanoStringNorm.test.data$outputs$top.geo.mean);
+        }
+    );
+
+test_that(
+    'sample.content.normalization regression case "total.sum"', {
+        load('data/NanoStringNorm.Rda');
+
+        result <- sample.content.normalization(
+            NanoStringNorm.test.data$inputs$x,
+            NanoStringNorm.test.data$inputs$anno,
+            'total.sum',
+            verbose = FALSE
+            );
+
+        expect_equivalent(result, NanoStringNorm.test.data$outputs$total.sum);
+        }
+    );
